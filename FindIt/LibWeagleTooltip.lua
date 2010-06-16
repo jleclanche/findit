d4512930f5b0ec500ed94d68ef6fbbe555c6c16a
Jerome Leclanche <adys@azura>
2010-06-16 20:12:57 +0300

    Cleanup LibWeagleTooltip

--- FindIt/LibWeagleTooltip.lua
+++ FindIt/LibWeagleTooltip.lua
@@ -1,31 +1,34 @@
-------------
--- FindIt --
-------------
+----------------------
+-- LibWeagleTooltip --
+----------------------
 -- Easily scan and match a tooltip's lines
 -- Feedback, questions, adys@mmo-champion.com
 
 -- LibWeagleTooltip is licensed under BSD
 -- Please read the LICENSE file for details
 
-local WeagleLibTooltip = CreateFrame("GameTooltip", "WeagleLibTooltip", UIParent, "GameTooltipTemplate")
-WeagleLibTooltip:SetOwner(UIParent, "ANCHOR_PRESERVE")
-WeagleLibTooltip:SetPoint("CENTER", "UIParent")
-WeagleLibTooltip:Hide()
+LibWeagleTooltip = {}
+LibWeagleTooltip.tt = CreateFrame("GameTooltip", "WeagleLibTooltip", UIParent, "GameTooltipTemplate")
+local LWTT = LibWeagleTooltip.tt
 
-local function settooltiphack(link) -- XXX
-	WeagleLibTooltip:SetOwner(UIParent, "ANCHOR_PRESERVE")
-	WeagleLibTooltip:SetHyperlink("spell:1")
-	WeagleLibTooltip:Show()
-	WeagleLibTooltip:SetHyperlink(link)
+LibWeagleTooltip.tt:SetOwner(UIParent, "ANCHOR_PRESERVE")
+LibWeagleTooltip.tt:SetPoint("CENTER", "UIParent")
+LibWeagleTooltip.tt:Hide()
+
+function LibWeagleTooltip:Prepare(link)
+	LWTT:SetOwner(UIParent, "ANCHOR_PRESERVE")
+	LWTT:SetHyperlink("spell:1:LibWeagleTooltip")
+	LWTT:Show()
+	LWTT:SetHyperlink(link)
 end
 
-local function unsettooltiphack() -- XXX
-	WeagleLibTooltip:SetOwner(UIParent, "ANCHOR_PRESERVE")
-	WeagleLibTooltip:Hide()
+function LibWeagleTooltip:HideTooltip()
+	LWTT:SetOwner(UIParent, "ANCHOR_PRESERVE")
+	LWTT:Hide()
 end
 
-function ScanTooltip(link)
-	settooltiphack(link)
+function LibWeagleTooltip:ScanTooltip(link)
+	self:Prepare(link)
 	
 	local lines = WeagleLibTooltip:NumLines()
 	local tooltiptxt = ""
@@ -46,31 +49,31 @@ function ScanTooltip(link)
 		end
 	end
 	
-	unsettooltiphack()
+	self:Hide()
 	return tooltiptxt
 end
 
-function GetTooltipLine(link, line, side)
+function LibWeagleTooltip:GetTooltipLine(link, line, side)
 	side = side or "Left"
-	settooltiphack(link)
+	self:Prepare(link)
 	
 	local lines = WeagleLibTooltip:NumLines()
-	if line > lines then return unsettooltiphack() end
+	if line > lines then return self:Hide() end
 	
 	local text = _G["WeagleLibTooltipText"..side..line]:GetText()
 	
-	unsettooltiphack()
+	self:Hide()
 	return text
 end
 
-function GetTooltipLines(link, ...)
+function LibWeagleTooltip:GetTooltipLines(link, ...)
 	local lines = {}
-	settooltiphack(link)
+	self:Prepare(link)
 	
 	for k,v in pairs({...}) do
 		lines[#lines+1] = _G["WeagleLibTooltipTextLeft"..v]:GetText()
 	end
 	
-	unsettooltiphack()
+	self:Hide()
 	return unpack(lines)
 end
