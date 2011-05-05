86d92446e534a24ff91e73035439d2741db51bce
Jerome Leclanche <adys@azura>
2011-05-05 04:03:08 +0100

    Add a release script

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TOC="FindIt/FindIt.toc"
+VERSIONTAG="## Version: "
+VERSION=$(grep "$VERSIONTAG" $TOC | sed "s/$VERSIONTAG//")
+OUTFILE="findit-$VERSION.zip"
+
+git archive --format=zip origin FindIt/ > $OUTFILE &&
+echo $OUTFILE
