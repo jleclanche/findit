6c5f60f40b5270e8b1ef66b6229da4b54f8f16f4
Jerome Leclanche <jerome@leclan.ch>
2014-07-27 23:02:58 +0100

    Avoid duplicating Version

--- FindIt/FindIt.lua
+++ FindIt/FindIt.lua
@@ -11,7 +11,7 @@
 FindIt = select(2, ...)
 FindIt.NAME = select(1, ...)
 FindIt.CNAME = "|cff33ff99" .. FindIt.NAME .. "|r"
-FindIt.VERSION = "1.10.0"
+FindIt.VERSION = GetAddOnMetadata("FindIt", "Version")
 FindIt.commands = {}
 FindIt.MAXRESULTS = 1000
 
